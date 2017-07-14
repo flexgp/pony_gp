ff45bf5af094081879f9ba9851d4c30324346355
Erik Hemberg <erik.hemberg@gmail.com>
2017-07-14 12:49:04 +0100

    Adding test for main

---
+++ test_pony_gp.py
@@ -0,0 +1,25 @@
+import sys
+import unittest
+import pony_gp
+
+
+class TestPonyGP(unittest.TestCase):
+    
+    def test_one_main(self):
+        """Test the `main` method of `pony_gp` with the `configs.ini` file
+
+        """
+        args = [
+            'pony_gp.py',
+            '--config=configs.ini'
+            ]
+        sys.argv = args
+        pony_gp.main()
+        self.assertTrue(True)
+
+        
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(TestPonyGP('test_one_main'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
