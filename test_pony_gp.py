3e2c46e4ed28490a1f6ca10cac5b634ee8fe827c
Erik Hemberg <erik.hemberg@gmail.com>
2020-06-06 10:27:30 -0400

    Add typing

--- test_pony_gp.py
+++ test_pony_gp.py
@@ -5,7 +5,7 @@ import pony_gp
 
 class TestPonyGP(unittest.TestCase):
     
-    def test_one_main(self):
+    def test_one_main(self) -> None:
         """Test the `main` method of `pony_gp` with the `configs.ini` file
 
         """
